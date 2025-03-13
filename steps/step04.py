b5d6129232b3add65cc8dfa4d9113cb142f94fde
wooseungw <woow070840@gmail.com>
2025-03-13 13:21:20 +0900

    기본 변수 및 함수 클래스 구현, 제곱, 지수, 1 더하기 기능 추가

---
+++ steps/step04.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        self.input = input
+        self.output = output
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+
+class AddOne(Function):
+    def forward(self, x):
+        return x + 1
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+def diff(x):
+    return 2*(1+np.exp(x))*np.exp(x)
+
+print(diff(1))
+
+if __name__ == '__main__':
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+    print(y.data)
+
+    y.grad = np.array(1.0)
+    y.backward()
+    print(x.grad)
