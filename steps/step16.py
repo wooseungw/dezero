883766c62e31a5220be3d0d47277501f8f19bdd7
wooseungw <woow070840@gmail.com>
2025-04-03 13:23:23 +0900

    add step20

--- steps/step16.py
+++ steps/step16.py
@@ -10,9 +10,11 @@ class Variable:
         self.data = data
         self.grad = None
         self.creator = None
+        self.generation = 0
 
     def set_creator(self, func):
         self.creator = func
+        self.generation = func.generation + 1
 
     def cleargrad(self):
         self.grad = None
@@ -21,7 +23,17 @@ class Variable:
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
-        funcs = [self.creator]
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
         while funcs:
             f = funcs.pop()
             gys = [output.grad for output in f.outputs]
@@ -36,7 +48,7 @@ class Variable:
                     x.grad = x.grad + gx
 
                 if x.creator is not None:
-                    funcs.append(x.creator)
+                    add_func(x.creator)
 
 
 def as_array(x):
@@ -53,6 +65,7 @@ class Function:
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
 
+        self.generation = max([x.generation for x in inputs])
         for output in outputs:
             output.set_creator(self)
         self.inputs = inputs
@@ -64,6 +77,21 @@ class Function:
 
     def backward(self, gys):
         raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+
+def square(x):
+    return Square()(x)
 
 
 class Add(Function):
@@ -79,13 +107,10 @@ def add(x0, x1):
     return Add()(x0, x1)
 
 
-x = Variable(np.array(3.0))
-y = add(x, x)
+x = Variable(np.array(2.0))
+a = square(x)
+y = add(square(a), square(a))
 y.backward()
-print(x.grad)
 
-
-x = Variable(np.array(3.0))  
-y = add(add(x, x), x)
-y.backward()
+print(y.data)
 print(x.grad)
