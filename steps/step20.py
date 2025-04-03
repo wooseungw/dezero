9972d715c5c7913a9afe32da688bad457ef2afc8
wooseungw <woow070840@gmail.com>
2025-04-03 13:23:55 +0900

    minor update

--- steps/step20.py
+++ steps/step20.py
@@ -36,15 +36,12 @@ class Variable:
     @property
     def shape(self):
         return self.data.shape
-
     @property
     def ndim(self):
         return self.data.ndim
-
     @property
     def size(self):
         return self.data.size
-
     @property
     def dtype(self):
         return self.data.dtype
