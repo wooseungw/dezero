23e2d1acf898867f46283ff8b8dbd252f102ea44
wooseungw <woow070840@gmail.com>
2025-04-15 23:32:16 +0900

    beale 함수 추가 및 goldstein 함수 호출을 beale로 변경

--- steps/step24.py
+++ steps/step24.py
@@ -20,9 +20,12 @@ def goldstein(x, y):
         (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
     return z
 
+def beale(x,y):
+    z  = (1.5 - x + x*y)**2 + (2.25 - x + x*(y**2))**2 + (2.625 - x + x*(y**3))**2
+    return z
 
-x = Variable(np.array(3.0))
-y = Variable(np.array(1.0))
-z = goldstein(x, y)  
+x = Variable(np.array(1.0))
+y = Variable(np.array(0.0))
+z = beale(x, y)  
 z.backward()
 print(x.grad, y.grad)
