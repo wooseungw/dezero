54de73abcb033420b68a438952166d8c0adfe26f
wooseungw <woow070840@gmail.com>
2025-05-01 12:44:22 +0900

    is_simple_core 값을 False로 변경하고 core.py 파일 추가 utils.py의 _dot_var 함수에서 변수 이름을 출력할 때 shape 대신 변수 값을 사용하도록 수정

--- dezero/utils.py
+++ dezero/utils.py
@@ -8,7 +8,7 @@ def _dot_var(v, verbose=False):
     if verbose and v.data is not None:
         if v.name is not None:
             name += ': '
-        name += str(v.shape) + ' ' + str(v.dtype)
+        name += str(v) + ' ' + str(v.dtype)
 
     return dot_var.format(id(v), name)
 
